f37432d9853096340d6de156282095872892203f
QueenlyCrimson <ccinemainn@gmail.com>
2023-06-13 13:28:01 -0500

    Components adjusted

---
+++ my-portfolio/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+
+const NavBar = () => {
+  
+
+  return(
+    <header>
+      <nav>
+        <div>
+          <h1>Erin Keys</h1>
+          <h1>Home</h1>
+          <h1>Projects</h1>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
