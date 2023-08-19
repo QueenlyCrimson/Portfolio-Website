3e2de10f7c3fad7e34fc0faa201434c7f7a3c637
QueenlyCrimson <ccinemainn@gmail.com>
2023-08-19 14:49:31 -0500

    projectImg styles added

--- my-portfolio/src/components/ProjectMemory.jsx
+++ my-portfolio/src/components/ProjectMemory.jsx
@@ -5,7 +5,7 @@ const ProjectMemory = () => {
       <a href="https://detective-memory-card-game.onrender.com/game.html">
       <button className="projectholder" >
         <div className="projectname">Memory Flash Card Game</div>
-        <div className="projectimageholder"><img src="https://cdn.discordapp.com/attachments/702380650455367806/1119395340458668232/ezgif-2-d888b4cf42.gif" alt="yeah"></img></div>
+        <div className="projectimageholder"><img className="projectImg" src="https://cdn.discordapp.com/attachments/702380650455367806/1119395340458668232/ezgif-2-d888b4cf42.gif" alt="yeah"></img></div>
         <div className="projectdescriptionholder">The Detective Memory Game features 10 cards which can be clicked and matched with other cards. It also features a shuffle button to reshuffle the game when finished.</div>
       </button>
       </a>
