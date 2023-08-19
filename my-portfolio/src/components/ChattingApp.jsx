4b7ed19c1b2235cfd3e980ddf17b89f3ee19b4f5
QueenlyCrimson <ccinemainn@gmail.com>
2023-08-19 14:22:43 -0500

    ChattingApp component added

---
+++ my-portfolio/src/components/ChattingApp.jsx
@@ -0,0 +1,16 @@
+const ChattingApp = () => {
+
+  return(
+    <div>
+      <a href="https://lantern-chatting-app.onrender.com/">
+      <button className="projectholder" >
+        <div className="projectname">Lantern Chatting</div>
+        <div className="projectimageholder"><img src="https://cdn.discordapp.com/attachments/710881503592185867/1142534679006814318/ezgif.com-video-to-gif_1.gif" alt="yeah"></img></div>
+        <div className="projectdescriptionholder">Lantern chatting uses web sockets to allow people to chat with each other over the internet using Websockets and an Express based backend.</div>
+      </button>
+      </a>
+    </div>
+  )
+}
+
+export default ChattingApp
